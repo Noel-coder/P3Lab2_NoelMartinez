523da7623cd0810600329c1048d53e966527b4a7
noel-coder <noelgg_23@outlook.es>
2021-04-30 16:59:02 -0600

    Disculpame, soy maje(lo averiguaras en el ejercicio 3 XD)

--- main.cpp
+++ main.cpp
@@ -23,15 +23,25 @@ void mostrarArreglo(int arreglo[], int tamano) {
 		cout << "[" << arreglo[i]<<"]";
 	cout << endl;
 }
+void RecursivaPascal(int n, int max_n, int res_anterior[], int salida[]) {
+	if(n==max_n) {
 
-int RecursivaPascal(int n, int max_n, int res_anterior[], int salida[],int tamano) {
-	if(n==0 || n==max_n)
-		return 1;
-	else if(n==1 || (n+1)==max_n) {
-
+	} else {
+		for(int i=0; i < n ; i++) {
+			if(i == 0 || i==n-1) {
+				salida[i]=1;
+			} else {
+				salida[i]= res_anterior[i-1] + res_anterior[i];
+			}
+		}
+		for(int i=0; i < n; i++) {
+			res_anterior[i]=salida[i];
+			cout << salida [i] << " ";
+			salida[i]=0;
+		}
+		cout << endl;
+		RecursivaPascal(n+1,max_n,res_anterior,salida);
 	}
-	return max_n;
-	//else return RecursivaPascal(max_n-1, n-1,res_anterior[100],salida[100])+RecursivaPascal(max_n-1,n,res_anterior[100],salida[100]));
 }
 
 void GraficoBarra() {
@@ -47,72 +57,74 @@ void GraficoBarra() {
 	mostrarArreglo(arreglo1,tamano);
 	cout<< "Arreglo 2: ";
 	mostrarArreglo(arreglo2,tamano);
-	int cont=1;
+	//JAJAJAJ disculpame me mame :v lo pude hacer en 10 lineas pero era muy tarde 
 	for(int i=0; i < tamano; i++) {
+		cout<<i+1<<"- "<<endl;
 		switch(arreglo1[i]) {
 			case 1: {
-				cout<< caracterClaro;
+				cout<< caracterClaro << " ";
+				cout<<endl;
 				break;
 			}
 			case 3: {
 				for(int i=0; i < 3; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 5: {
 				for(int i=0; i < 5; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 7: {
 				for(int i=0; i < 7; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 9: {
 				for(int i=0; i < 9; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 11: {
 				for(int i=0; i < 11; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 13: {
 				for(int i=0; i < 13; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 15: {
 				for(int i=0; i < 15; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 17: {
 				for(int i=0; i < 17; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 19: {
 				for(int i=0; i < 19; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
@@ -122,195 +134,192 @@ void GraficoBarra() {
 		switch(arreglo1[i]) {
 			case 2: {
 				for(int i=0; i < 2; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 4: {
 				for(int i=0; i < 4; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 6: {
 				for(int i=0; i < 6; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 8: {
 				for(int i=0; i < 8; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 10: {
 				for(int i=0; i < 10; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 12: {
 				for(int i=0; i < 12; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 14: {
 				for(int i=0; i < 14; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 16: {
 				for(int i=0; i < 16; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 18: {
 				for(int i=0; i < 18; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 			case 20: {
 				for(int i=0; i < 20; i++) {
-					cout<< caracterClaro;
+					cout<< caracterClaro << " ";
 				}
 				cout<<endl;
 				break;
 			}
 		}
-		//cout<<endl;
 		//Arreglo2
 		switch(arreglo2[i]) {
 			case 1: {
-				cout<< caracterOscuro;
+				cout<< caracterOscuro << " ";
 				cout<<endl;
 				cout<<endl;
 				break;
 			}
 			case 3: {
 				for(int i=0; i < 3; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
-					cout<<endl;
-					cout<<endl;
+				cout<<endl;
+				cout<<endl;
 				break;
 			}
 			case 5: {
 				for(int i=0; i < 5; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
-					cout<<endl;
-					cout<<endl;
+				cout<<endl;
+				cout<<endl;
 				break;
 			}
 			case 7: {
 				for(int i=0; i < 7; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
-					cout<<endl;
-					cout<<endl;
+				cout<<endl;
+				cout<<endl;
 				break;
 			}
 			case 9: {
 				for(int i=0; i < 9; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
-					cout<<endl;
-					cout<<endl;
+				cout<<endl;
+				cout<<endl;
 				break;
 			}
 			case 11: {
 				for(int i=0; i < 11; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
-					cout<<endl;
-					cout<<endl;
+				cout<<endl;
+				cout<<endl;
 				break;
 			}
 			case 13: {
 				for(int i=0; i < 13; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
-					cout<<endl;
-					cout<<endl;
+				cout<<endl;
+				cout<<endl;
 				break;
 			}
 			case 15: {
 				for(int i=0; i < 15; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
-					cout<<endl;
-					cout<<endl;
+				cout<<endl;
+				cout<<endl;
 				break;
 			}
 			case 17: {
 				for(int i=0; i < 17; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
-					cout<<endl;
-					cout<<endl;
+				cout<<endl;
+				cout<<endl;
 				break;
 			}
 			case 19: {
 				for(int i=0; i < 19; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
-					cout<<endl;
-					cout<<endl;
+				cout<<endl;
+				cout<<endl;
 				break;
 			}
 		}
-		//cout<<endl;
-		//cout<<endl;
 		switch(arreglo2[i]) {
 			case 2: {
 				for(int i=0; i < 2; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
-					cout<<endl;
-					cout<<endl;
+				cout<<endl;
+				cout<<endl;
 				break;
 			}
 			case 4: {
 				for(int i=0; i < 4; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
-					cout<<endl;
-					cout<<endl;
+				cout<<endl;
+				cout<<endl;
 				break;
 			}
 			case 6: {
 				for(int i=0; i < 6; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
-					cout<<endl;
-					cout<<endl;
+				cout<<endl;
+				cout<<endl;
 				break;
 			}
 			case 8: {
 				for(int i=0; i < 8; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
-					cout<<endl;
-					cout<<endl;
+				cout<<endl;
+				cout<<endl;
 				break;
 			}
 			case 10: {
 				for(int i=0; i < 10; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
 				cout<<endl;
 				cout<<endl;
@@ -318,7 +327,7 @@ void GraficoBarra() {
 			}
 			case 12: {
 				for(int i=0; i < 12; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
 				cout<<endl;
 				cout<<endl;
@@ -326,7 +335,7 @@ void GraficoBarra() {
 			}
 			case 14: {
 				for(int i=0; i < 14; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
 				cout<<endl;
 				cout<<endl;
@@ -334,7 +343,7 @@ void GraficoBarra() {
 			}
 			case 16: {
 				for(int i=0; i < 16; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
 				cout<<endl;
 				cout<<endl;
@@ -342,7 +351,7 @@ void GraficoBarra() {
 			}
 			case 18: {
 				for(int i=0; i < 18; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
 				cout<<endl;
 				cout<<endl;
@@ -350,15 +359,13 @@ void GraficoBarra() {
 			}
 			case 20: {
 				for(int i=0; i < 20; i++) {
-					cout<< caracterOscuro;
+					cout<< caracterOscuro << " ";
 				}
 				cout<<endl;
 				cout<<endl;
 				break;
 			}
 		}
-		//cout<<endl;
-		//cout<<endl;
 	}
 }
 
@@ -399,16 +406,18 @@ int main(int argc, char** argv) {
 		switch(opcion=menu()) {
 			case 1: {
 				//triangulo de pascal
-				int max_n,tamano=100;
+				int max_n,n=1;
+				int res_anterior[100];
+				int salida[100];
 				cout << "Ingrese el valor maximo: "<< endl;
 				cin >> max_n;
-				while(max_n < 0 || max_n >= 100) {
+				while(max_n <= 0 || max_n >= 100) {
 					cout << "El dato introducido no es valido, intentelo de nuevo!" << endl;
 					cout << "Ingrese el valor maximo: "<< endl;
 					cin >> max_n;
 				}
-				int res_anterior[tamano];
-				int salida[tamano];
+				cout<<"Triangulo de Pascal:"<<endl;
+				RecursivaPascal(n,max_n+2,res_anterior,salida);
 				break;
 			}
 			case 2: {
